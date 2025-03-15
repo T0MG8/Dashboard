0ea863ffd6553feb902972b32b028dd5efdeb530
T0MG8 <tom.goede@hva.nl>
2025-03-15 15:11:49 +0100

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import streamlit as st
-import plotly
-import plotly.graph_objects as go
+from plotly import graph_objects as go
 
 # Data inladen
 afspraken2020 = pd.read_excel('./Data/afspraken 2020.xlsx') 
