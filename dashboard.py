a9529bc6036d75a45192bb338af253df867e6942
T0MG8 <tom.goede@hva.nl>
2025-03-17 18:43:11 +0100

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,8 @@ import streamlit as st
 import plotly.graph_objects as go
 import plotly.express as px
 
+pagina = st.sidebar.radio("Selecteer een pagina", ['Afspraken', 'Factuur'])
+
 # Data inladen
 @st.cache_data
 def load_data_afspraken():
@@ -32,45 +34,47 @@ dataframes = {
     2024: afspraken2024
 }
 
-# Plotly figuur
-fig = go.Figure()
+if pagina == 'Afspraken':
+    # Plotly figuur
+    fig = go.Figure()
 
-# Voor elke dataframe een lijn toevoegen
-for year, df in dataframes.items():
-    # Datum kolom omzetten naar datetime-formaat
-    df['datum'] = pd.to_datetime(df['datum'], format='%d-%m-%Y')
-    
-    # Maand extracten
-    df['maand'] = df['datum'].dt.month
-    
-    # Afspraken per maand tellen
-    maand_telling = df['maand'].value_counts().sort_index()
-    
-    # Plot toevoegen
-    fig.add_trace(go.Scatter(
-        x=maand_telling.index,
-        y=maand_telling.values,
-        mode='lines+markers',
-        name=f'{year}'
-    ))  
+    # Voor elke dataframe een lijn toevoegen
+    for year, df in dataframes.items():
+        # Datum kolom omzetten naar datetime-formaat
+        df['datum'] = pd.to_datetime(df['datum'], format='%d-%m-%Y')
+        
+        # Maand extracten
+        df['maand'] = df['datum'].dt.month
+        
+        # Afspraken per maand tellen
+        maand_telling = df['maand'].value_counts().sort_index()
+        
+        # Plot toevoegen
+        fig.add_trace(go.Scatter(
+            x=maand_telling.index,
+            y=maand_telling.values,
+            mode='lines+markers',
+            name=f'{year}'
+        ))  
 
-fig.update_traces(hovertemplate='<br>Aantal: %{y}<br>')
+    fig.update_traces(hovertemplate='<br>Aantal: %{y}<br>')
 
-# Layout aanpassen
-fig.update_layout(
-    title="Aantal afspraken per maand",
-    xaxis=dict(title="Maand", tickmode='array', showgrid=True, tickvals=list(range(1, 13)), ticktext=['Jan', 'Feb', 'Mrt', 'Apr', 'Mei', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec']),
-    yaxis_title="Aantal afspraken",
-    legend_title="Jaar",
-    yaxis=dict(showgrid=True)
+    # Layout aanpassen
+    fig.update_layout(
+        title="Aantal afspraken per maand",
+        xaxis=dict(title="Maand", tickmode='array', showgrid=True, tickvals=list(range(1, 13)), ticktext=['Jan', 'Feb', 'Mrt', 'Apr', 'Mei', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec']),
+        yaxis_title="Aantal afspraken",
+        legend_title="Jaar",
+        yaxis=dict(showgrid=True)
     )
 
-# Streamlit plotten
-st.plotly_chart(fig)
+    # Streamlit plotten
+    st.plotly_chart(fig)
 
 
+elif pagina == 'Factuur':
 # Zorg ervoor dat factuurdatum in datetime-formaat is
-factuur['factuurdatum'] = pd.to_datetime(factuur['factuurdatum'], dayfirst=True)
+    factuur['factuurdatum'] = pd.to_datetime(factuur['factuurdatum'], dayfirst=True)
 
 # Extraheer jaar en maand
 factuur['jaar'] = factuur['factuurdatum'].dt.year
